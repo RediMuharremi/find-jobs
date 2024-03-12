b5b7e1fe9d0f388c2449e6d9a64d272a2f110e55
Redi Muharremi <r.muharremi@lah-group.net>
2024-03-12 14:44:07 +0100

    add project files

--- download_out.py
+++ download_out.py
@@ -2,7 +2,7 @@ import gdown
 import os
 
 def ddd():
-    url = 'https://drive.google.com/uc?id=1BzzdNI_7jkvYXxwLpJ7si4vusj0bbL-C'  # Make sure to replace 'FILE_ID' with the actual file ID
+    url = 'https://drive.google.com/uc?id=1W1BT4MTJotZNPxSyNXf-AnuYGRqTxGEG'  # Make sure to replace 'FILE_ID' with the actual file ID
     output = 'daily_active_jobs_v3.csv'  # Optional: specify the name of the file to save it as
     file_exists = os.path.exists(output)
     if file_exists:
